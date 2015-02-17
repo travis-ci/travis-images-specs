1e94b0205c1374e88ebcf892640ac4ecf150c37d
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-17 15:17:22 +0100

    add a test for ruby methods

--- spec/localhost/ruby_spec.rb
+++ spec/localhost/ruby_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
-describe 'Installation' do
-  it 'expects the correct ruby version to be installed' do
-    expect(RUBY_VERSION).to include('2.1.2')
-  end
-
-  describe command('ruby -v') do
-    its(:stdout) { should match '2.1.2' }
-  end
-
-  describe command('rvm list default') do
+describe 'Ruby installation' do
+  skip command('rvm list default') do
     its(:stdout) { should match '1.9.3' }
+  end
+
+  describe command('rvm list known') do
+    its(:stdout) { should match '1.9.3' }
+  end
+
+  describe 'Ruby command is executed' do
+    describe command('ruby -e \'puts \"Konstanin broke all the things!\"\'') do
+      its(:stdout) { should match 'Konstanin broke all the things' }
+    end
   end
 end
