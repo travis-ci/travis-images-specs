2c47e06b0247e323aedd7c365868a8b02e8a2de0
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-17 13:30:32 +0100

    update test

--- spec/localhost/ruby_spec.rb
+++ spec/localhost/ruby_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe 'Installation' do
   it 'expects the correct ruby version to be installed' do
-    expect(RUBY_VERSION).to include('ruby 1.9.3')
+    expect(RUBY_VERSION).to include('2.1.2')
+  end
+
+  describe command('ruby -v') do
+    its(:stdout) { should match '2.1.2' }
+  end
+
+  describe command('rvm list default') do
+    its(:stdout) { should match '1.9.3' }
   end
 end
