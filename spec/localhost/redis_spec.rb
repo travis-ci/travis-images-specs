90bef6605909729a0f23b863901326d3f66bbb0a
carlad <carla@travis-ci.org>
2015-02-18 13:37:19 +0100

    improve redis spec

--- spec/localhost/redis_spec.rb
+++ spec/localhost/redis_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe 'redis installation' do
 
+  before :all do
+    system('redis-server &')
+  end
+
   describe command ('redis-cli ping') do
     its(:stdout) { should match 'PONG' }
   end
 
-  # describe command('createdb test_db; psql --list') do
-  #   its(:stdout) { should match 'test_db' }
-  # end
+  describe command ('redis-cli set key test_key; redis-cli get key') do
+    its(:stdout) { should match 'test_key' }
+  end
 
-  # describe command('psql -c "CREATE TABLE test_table();" test_db; psql -c "\dt" test_db') do
-  #   its(:stdout) { should match 'test_table'}
-  # end
 end
