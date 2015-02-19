76cfa76a35ea4c206c48ef5ec8a676862ef89f87
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-19 11:17:05 +0100

    start mongo

--- spec/localhost/mongodb_spec.rb
+++ spec/localhost/mongodb_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe 'mongodb installation' do
 
-  #describe package('mongo') do
-    #it { should be_installed }
-  #end
+  describe 'mongo commands', sudo: true do
 
-  describe command ('mongo --version') do
-    its(:stdout) { should match 'mongoDB shell version' }
+    describe command ('mongo --version')  do
+      its(:stdout) { should match 'MongoDB shell version' }
+    end
+
+    before do
+      system('sudo service mongodb start')
+    end
+
+    describe command ('cat /var/log/mongodb/mongodb.log')  do
+      its(:stdout) { should match '[initandlisten] waiting for connections on port' }
+    end
+
   end
 
 end
