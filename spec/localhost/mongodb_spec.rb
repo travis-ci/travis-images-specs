f378e7411b27fa947a7f22a9cc9e3b5e40994d54
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-19 12:27:31 +0100

    add test collection to mongo

--- spec/localhost/mongodb_spec.rb
+++ spec/localhost/mongodb_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'mongodb installation' do
 
+  describe command ('mongo --version')  do
+    its(:stdout) { should match 'MongoDB shell version' }
+  end
+
   describe 'mongo commands', sudo: true do
 
-    describe command ('mongo --version')  do
-      its(:stdout) { should match 'MongoDB shell version' }
-    end
-
-    before do
+    before :all do
       system('sudo service mongodb start')
       system('sleep 10')
     end
@@ -17,6 +17,9 @@ describe 'mongodb installation' do
       its(:stdout) { should match '[initandlisten] waiting for connections on port' }
     end
 
+    describe command ('mongo --eval "db.testData.insert( { x : 6 } ); db.getCollectionNames()"')  do
+      its(:stdout) { should match 'testData' }
+    end
   end
 
 end
