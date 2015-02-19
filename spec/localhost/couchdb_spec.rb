8e5fbe4c80c630a533a81ef2aff63aa9e3a189da
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-19 15:31:41 +0100

    add a table with content

--- spec/localhost/couchdb_spec.rb
+++ spec/localhost/couchdb_spec.rb
@@ -12,15 +12,21 @@ describe 'couchdb installation' do
       system('sudo service couchdb start')
       system('sleep 5')
       system('curl -X PUT http://127.0.0.1:5984/baseball')
+      system('curl -X PUT http://127.0.0.1:5984/baseball/bat -H 'Content-Type: application/json' -d '{"Name":"Testname"}'')
     end
 
     describe command ('curl http://127.0.0.1:5984/') do
-      its(:stdout) { should contain 'couchdb Welcome' }
+      its(:stdout) { should match '"couchdb":"Welcome"' }
     end
 
     describe command ('curl -X GET http://127.0.0.1:5984/_all_dbs') do
       its(:stdout) { should match 'baseball' }
     end
+
+    describe command ('curl -X GET http://127.0.0.1:5984/baseball/bat') do
+      its(:stdout) { should match '{"_id":"bat","Name":"Testname"' }
+    end
+
 
   end
 
