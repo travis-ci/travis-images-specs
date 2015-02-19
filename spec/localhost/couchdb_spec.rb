faa865dca0b99d248c6d56dda85f20f03ae1d5eb
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-19 14:57:39 +0100

    start couchdb with sudo

--- spec/localhost/couchdb_spec.rb
+++ spec/localhost/couchdb_spec.rb
@@ -6,4 +6,17 @@ describe 'couchdb installation' do
     it { should be_installed }
   end
 
+  describe 'couchdb commands', sudo: true do
+
+    before :all do
+      system('sudo service couchdb start')
+      system('sleep 5')
+    end
+
+    describe command ('curl http://127.0.0.1:5984/') do
+      its(:stdout) { should include 'couchdb Welcome' }
+    end
+
+  end
+
 end
