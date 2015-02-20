dd6e4a9c9e09f29b0c0f70ffdba4ce99b474803e
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-20 13:09:03 +0100

    add enabled

--- spec/localhost/memcached_spec.rb
+++ spec/localhost/memcached_spec.rb
@@ -6,8 +6,12 @@ describe 'memcached installation' do
     it { should be_installed }
   end
 
+  describe service('memcached') do
+    it { should be_running }
+  end
+
   describe command ('echo \'stats\' | nc 127.0.0.1 11211')  do
-    its(:stdout) { should match 'version' }
+    its(:echo) { should match 'version' }
   end
 
 end
