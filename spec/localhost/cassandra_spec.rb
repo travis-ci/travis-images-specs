fcda558c8ee0493286630efd21241a0980f36e9c
carlad <carla@travis-ci.org>
2015-02-23 12:14:23 +0100

    add command tests for cassandra

--- spec/localhost/cassandra_spec.rb
+++ spec/localhost/cassandra_spec.rb
@@ -21,22 +21,12 @@ describe 'cassandra installation' do
       its(:stdout) { should match 'Connected to: \"Test Cluster\" on localhost/9160' }
     end
 
-    # describe command ('cassandra-cli "connect localhost/9160;" 
-    #                     "create keyspace Twissandra;" 
-    #                     "use Twissandra;" 
-    #                     "create column family User with comparator = UTF8Type;" 
-    #                     "update column family User with column_metadata =
-    #                     [
-    #                     {column_name: first, validation_class: UTF8Type},
-    #                     {column_name: last, validation_class: UTF8Type},
-    #                     {column_name: age, validation_class: UTF8Type, index_type: KEYS}
-    #                     ];" 
-    #                     "assume User keys as utf8;" 
-    #                     "set User['adakota']['first'] = 'Allen';
-    #                     set User['adakota']['last'] = 'Dakota';
-    #                     set User['adakota']['age'] = '45';" 
-    #                     "get User['adakota'];"')do
-    #   its(:stdout) { should include( 'name=age', 'value=45', 'name=first', 'value=Allen', 'name=last', 'value=Dakota' ) }
-    # end
+    describe command ('cassandra-cli -host localhost -port 9160 -f ../scripts/cassandra_schema.txt') do
+      its(:stdout) { should include( 'Value inserted', 'Returned 3 results', 'name=age', 'value=45', 'name=first', 'value=Allen', 'name=last', 'value=Dakota' ) }
+    end
   end
-end
+
+
+
+
+
