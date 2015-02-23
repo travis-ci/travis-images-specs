6245e6edeb496232d8d88e06fdd13be6de2086cb
carlad <carla@travis-ci.org>
2015-02-23 17:56:52 +0100

    remove unnecessary nodejs spec

--- spec/localhost/nodejs_spec.rb
+++ spec/localhost/nodejs_spec.rb
@@ -9,8 +9,4 @@ describe 'nodejs installation ' do
   describe command('node ./spec/files/node_server.js') do
     its(:stdout) { should match 'Server running at http://127.0.0.1:1337/'}
   end
-
-  describe command('node ./spec/files/node_http.js &; curl localhost:8080') do
-    its (:stdout) { should match 'Hello Http'}
-  end
 end
