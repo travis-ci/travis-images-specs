844521967e39206fb6ece9aac9250a416c168ed8
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-24 11:02:55 +0100

    add rabbitmqadmin

--- spec/localhost/rabbitmq_spec.rb
+++ spec/localhost/rabbitmq_spec.rb
@@ -24,6 +24,42 @@ describe 'rabbitmq installation' do
     describe command ('sudo rabbitmqctl status') do
       its(:stdout) { should include('Status of node rabbit@localhost','running_applications') }
     end
-  end
 
+    describe 'rabbitmqadmin commands' do
+
+      before do
+        system('.bin/rabbitmqadmin declare queue name=my-test-queue durable=false')
+        system('.bin/rabbitmqadmin publish exchange=amq.default routing_key=my-test-queue payload="hello, world" ')
+      end
+
+      describe command ('.bin/rabbitmqadmin list queues') do
+        its(:stdout) { should include('my-test-queue', '1') }
+      end
+
+      describe command ('.bin/rabbitmqadmin get queue=my-test-queue requeue=false') do
+        its(:stdout) { should include('my-test-queue', 'hello, world') }
+      end
+
+      describe command ('.bin/rabbitmqadmin list queues') do
+        its(:stdout) { should include('my-test-queue', '0') }
+      end
+
+    end
+  end
 end
+
+
+
+
+
+#sudo service rabbitmq-server start
+#curl https://raw.githubusercontent.com/rabbitmq/rabbitmq-management/rabbitmq_v3_4_4/bin/rabbitmqadmin > rabbitmqadmin
+#sudo chmod 755 rabbitmqadmin
+#./rabbitmqadmin declare queue name=my-new-queue durable=false
+#./rabbitmqadmin publish exchange=amq.default routing_key=my-new-queue payload="hello, world"
+#./rabbitmqadmin list queues
+##expect 1
+#./rabbitmqadmin get queue=my-new-queue requeue=false
+##expect payload
+#./rabbitmqadmin list queues
+##expect 0
