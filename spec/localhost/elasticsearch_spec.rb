fe4e44111d7bb2515e27d0d074f306b67422f5dc
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-24 14:51:43 +0100

    start elasticsearch

--- spec/localhost/elasticsearch_spec.rb
+++ spec/localhost/elasticsearch_spec.rb
@@ -5,6 +5,16 @@ describe 'elasticsearch installation' do
   describe package('elasticsearch') do
     it { should be_installed }
   end
+
+  describe 'elasticsearch commands', sudo: true do
+
+    before :all do
+      system('sudo service elasticsearch start')
+    end
+
+    describe command ('sudo service elasticsearch status') do
+      its(:stdout) { should match 'elasticsearch is running' }
+    end
 end
 
 
