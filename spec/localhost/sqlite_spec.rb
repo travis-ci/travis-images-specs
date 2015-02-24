4947b5f95843cc3b2c762afd80b03bcdc87c07af
carlad <carla@travis-ci.org>
2015-02-24 14:33:21 +0100

    add sqlite spec

---
+++ spec/localhost/sqlite_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'sqlite installation' do
+  describe command('sqlite3 -version') do
+    its(:stdout) { should match '3.7.9' }
+  end
+
+  describe 'sqlite commands are executed' do
+    describe command('sqlite3 test.db "CREATE TABLE Cars(Id INTEGER PRIMARY KEY, Name TEXT, Price INTEGER); 
+                      INSERT INTO "Cars" VALUES(1,'Audi',52642); 
+                      SELECT * FROM Cars;"') do
+      its(:stdout) { should match '1|Audi|52642' }
+    end
+  end
+end
