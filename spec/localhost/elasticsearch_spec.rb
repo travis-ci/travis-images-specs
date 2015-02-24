82886c6a0d3fafe8c29b12d313b697aa8a6dc3f5
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-24 15:08:25 +0100

    add sleep

--- spec/localhost/elasticsearch_spec.rb
+++ spec/localhost/elasticsearch_spec.rb
@@ -6,16 +6,19 @@ describe 'elasticsearch installation' do
     it { should be_installed }
   end
 
-  describe 'elasticsearch commands', sudo: true do
+  describe 'elasticsearch start', sudo: true do
 
     before :all do
       system('sudo service elasticsearch start')
+      system('sleep 5')
     end
 
     describe command ('sudo service elasticsearch status') do
       its(:stdout) { should match 'elasticsearch is running' }
     end
+
   end
+
 end
 
 
