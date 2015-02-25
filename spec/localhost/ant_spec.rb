652c78cca13846798c3c4fee1f4c11fb8319e959
carlad <carla@travis-ci.org>
2015-02-25 11:37:04 +0100

    improve ant spec

--- spec/localhost/ant_spec.rb
+++ spec/localhost/ant_spec.rb
@@ -6,9 +6,12 @@ describe 'ant installation' do
   end
 
   describe 'ant command is executed' do
-
     describe command('ant -diagnostics') do
       its(:stdout) { should include('------- Ant diagnostics report -------', 'ANT PROPERTIES', 'System Properties') }
+    end
+
+    describe command('ant &') do
+      its(:stdout) { should match 'Buildfile: build.xml does not exist! Build failed' }
     end
   end
 end
