d49535726caaada2c363cd605274a6afae87120e
carlad <carla@travis-ci.org>
2015-02-25 11:58:15 +0100

    fix ant spec

--- spec/localhost/ant_spec.rb
+++ spec/localhost/ant_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'ant installation' do
   describe command('ant -version') do
-    its(:stdout) { should match 'Apache Ant(TM) version 1.8.2 compiled on December 3 2011' }
+    its(:stdout) { should match 'Apache Ant(TM) version 1.8.2 compiled on December 3 2011\n' }
   end
 
   describe 'ant command is executed' do
-    describe command('ant -diagnostics') do
+    describe command('ant -diagnostics; sleep 5') do
       its(:stdout) { should include('Ant diagnostics report', 'ANT PROPERTIES', 'System Properties') }
     end
 
