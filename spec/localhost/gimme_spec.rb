68f0900010d3d56d8a5388cf3e15eb4843cde1f4
carlad <carla@travis-ci.org>
2015-02-25 15:38:22 +0100

    fix gimme spec

--- spec/localhost/gimme_spec.rb
+++ spec/localhost/gimme_spec.rb
@@ -6,15 +6,12 @@ describe 'gimme installation' do
   end
 end
 
-describe 'gimme command is executed' do
+describe 'gimme commands are executed' do
   describe command('gimme --list;') do
     its(:stdout) { should match '1.4.1 \<\= current' }
   end
 
-  describe command('go version') do
-    before do
-      system('gimme 1.3; source ~/.gimme/envs/go1.3.env; sleep 5')
-    end
+  describe command('gimme 1.3; source ~/.gimme/envs/go1.3.env; sleep 5; go version') do
     its(:stdout) { should match 'go version go1.3 linux/amd64' }
   end
 end
