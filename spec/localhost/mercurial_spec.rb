347c6a9747e43774332e79d1cb3e4ee15b4bf3a3
carlad <carla@travis-ci.org>
2015-02-26 15:37:15 +0100

    update mercurial spec

--- spec/localhost/mercurial_spec.rb
+++ spec/localhost/mercurial_spec.rb
@@ -7,18 +7,19 @@ describe 'mercurial installation' do
 end
 
 describe 'mecurial commands are executed' do
-  describe command('hg init test-project; sleep 3; cd test-project; sleep 3; touch test-file.txt; sleep 3; hg status') do
+  before do
+    system('hg init test-project; sleep 3; cd test-project; sleep 3; touch test-file.txt')
+  end
+
+  describe command('hg status') do
     its(:stdout) { should match '\? test-file.txt' }
   end
 
-  # describe command('hg add .; hg status') do
-  #   its(:stdout) { should match 'A test-file.txt' }
-  # end
+  describe command('hg add .; hg status') do
+    its(:stdout) { should match '\A test-file.txt' }
+  end
 
-  #  describe command('hg forget test-file.txt; hg status') do
-  #   its(:stdout) { should match '$hg  test-file.txt' }
-  # end
-
-
-
+   describe command('hg add .; hg forget test-file.txt; hg status') do
+    its(:stdout) { should match '\? test-file.txt' }
+  end
 end
