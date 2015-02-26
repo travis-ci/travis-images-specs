4d2893e8ac41ce8d69e821e37aea87fd2bcf1594
carlad <carla@travis-ci.org>
2015-02-26 14:36:58 +0100

    add mecurial spec

---
+++ spec/localhost/mercurial_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'mercurial installation' do
+  describe command('hg version') do
+    its(:stdout) { should match 'Mercurial Distributed SCM \(version 2.0.2\)' }
+  end
+end
+
+describe 'mecurial commands are executed' do
+  describe command('hg init test-project; cd test-project; touch test-file.txt; hg status') do
+    its(:stdout) { should match '? test-file.txt' }
+  end
+
+  describe command('hg add .; hg status') do
+    its(:stdout) { should match 'A test-file.txt' }
+  end
+
+   describe command('hg forget test-file.txt; hg status') do
+    its(:stdout) { should match '$hg  test-file.txt' }
+  end
+
+
+
+end
