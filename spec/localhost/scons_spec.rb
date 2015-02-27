ca7f393b3a52d1a5dc667a6631d5cf934ca17fd8
carlad <carla@travis-ci.org>
2015-02-27 14:43:05 +0100

    improve scons spec

--- spec/localhost/scons_spec.rb
+++ spec/localhost/scons_spec.rb
@@ -14,4 +14,12 @@ describe 'scons commands are executed' do
                                   'gcc -o helloworld helloworld.o',
                                   'scons: done building targets.') }
   end
+  describe command('cd ./spec/files; scons --tree=all; sleep 5') do
+    its(:stdout) { should include('+-SConstruct',
+                                  '+-helloworld',
+                                  '+-helloworld.o',
+                                  '+-helloworld.c',
+                                  '+-/usr/bin/gcc',
+                                  'scons: done building targets.') }
+  end
 end
