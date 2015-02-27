c1ef0b1217a3a4ddd447f68a44fcf7c08b9f3bcd
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-27 10:48:42 +0100

    add Create Profile to the spec

--- spec/localhost/firefox_spec.rb
+++ spec/localhost/firefox_spec.rb
@@ -5,4 +5,17 @@ describe 'firefox installation' do
   describe package('elasticsearch') do
     it { should be_installed }
   end
+
+  describe 'firefox start' do
+
+    before :all do
+      system('DISPLAY=:99.0 sh -e /etc/init.d/xvfb start')
+      system('sleep 5')
+    end
+
+    describe command('firefox -CreateProfile test --display=DISPLAY=:99.0') do
+      its(:stdout) { should match 'Success' }
+    end
+
+  end
 end
