edc01d317ef96bb7dac50f771eb3e977dd9ee424
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-27 16:01:59 +0100

    add test

--- spec/localhost/phantomjs_spec.rb
+++ spec/localhost/phantomjs_spec.rb
@@ -6,4 +6,8 @@ describe 'phantomjs installation' do
     it { should be_installed }
   end
 
+  describe command ('phantomjs ./spec/files/phantomjs_test_google.js') do
+    its(:stdout) { should match 'Status: success' }
+  end
+
 end
