2633438667acf01992d4faffd3489a1db7a001f8
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-27 15:12:06 +0100

    kill firefox -9

--- spec/localhost/firefox_spec.rb
+++ spec/localhost/firefox_spec.rb
@@ -28,7 +28,7 @@ describe 'firefox installation' do
       system('DISPLAY=:99.0 sh -e /etc/init.d/xvfb start')
       system('firefox --display=DISPLAY=:99.0 https://www.mozilla.org')
       system('sleep 3')
-      system('pkill firefox') # here I need to exit out of the runnign browswer!
+      system('pkill -9 firefox') # here I need to exit out of the runnign browswer!
     end
 
     describe command('cd .mozilla/firefox/*.default; sqlite3 places.sqlite "SELECT * FROM moz_places;"') do
