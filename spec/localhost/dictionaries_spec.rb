d55dbea04eb7eac65ce324f04da0cafb87c40437
carlad <carlad@users.noreply.github.com>
2015-03-02 12:29:59 +0100

    add some tests to dictionaries spec

--- spec/localhost/dictionaries_spec.rb
+++ spec/localhost/dictionaries_spec.rb
@@ -4,4 +4,14 @@ describe 'dictionaries installation' do
   describe package('wamerican') do
     it { should be_installed }
   end
+  
+  describe 'dictionaries commands' do
+    describe command('look colonized') do
+      its(:stdout) { should match 'colonize\ncolonized\ncolonizer' }
+    end
+    
+    describe command('look colonised') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
