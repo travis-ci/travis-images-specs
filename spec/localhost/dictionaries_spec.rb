a9a1317f7171fa0af41ce09bdccb954b72001cd2
carlad <carlad@users.noreply.github.com>
2015-03-02 12:51:22 +0100

    Update dictionaries_spec.rb

--- spec/localhost/dictionaries_spec.rb
+++ spec/localhost/dictionaries_spec.rb
@@ -4,14 +4,14 @@ describe 'dictionaries installation' do
   describe package('wamerican') do
     it { should be_installed }
   end
+end
   
-  describe 'dictionaries commands' do
-    describe command('look colonized') do
-      its(:stdout) { should include('colonize', 'colonized', 'colonizer' }
-    end
-    
-    describe command('look colonised') do
-      its(:stdout) { should equal(nil) }
-    end
+describe 'dictionaries commands' do
+  describe command('look colonized') do
+    its(:stdout) { should include('colonize', 'colonized', 'colonizer') }
+  end
+  
+  describe command('look colonised') do
+    its(:stdout) { should match nil }
   end
 end
