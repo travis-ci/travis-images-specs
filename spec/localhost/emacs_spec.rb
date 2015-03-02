fec30c2959d07cb3ab8cba8aebc5ed8e5a94317d
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-02 11:51:19 +0100

    add butterblume.txt

--- spec/localhost/emacs_spec.rb
+++ spec/localhost/emacs_spec.rb
@@ -6,4 +6,19 @@ describe 'emacs installation' do
     its(:exit_status) { should eq 0 }
   end
 
+  describe 'read file with emacs' do
+    before do
+      system('echo "Butterblume" > butterblume.txt')
+    end
+
+    describe command('emacs --batch -l butterblume.txt') do
+      its(:stdout) { should match 'Butterblume' }
+    end
+  end
+
+#https://www.gnu.org/software/emacs/manual/html_node/emacs/Command-Example.html#Command-Example
+#
+#https://www.fnal.gov/docs/products/emacs/emacs/emacs_36.html
+
+
 end
