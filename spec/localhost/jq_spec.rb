e3faa4f49a16d4b4bcf9832d4c7d5524e4b98ce6
carlad <carlad@users.noreply.github.com>
2015-03-02 16:36:43 +0100

    add jq spec

---
+++ spec/localhost/jq_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'jq installation' do
+  describe command('jq -V') do
+    its(:stdout) { should match 'jq-1.4' }
+  end
+
+  describe 'jq command is executed' do
+    describe command('cat ./spec/files/jq.json | jq ".[0] | {message: .commit.message, name: .commit.committer.name}"') do
+      its(:stdout) { should match '"message": "Konstantin broke all the things.",\n"name": "Tyranja"' }
+    end
+  end
+end
