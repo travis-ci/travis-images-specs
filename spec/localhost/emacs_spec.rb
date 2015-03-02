6955b2332967c8bdac8102969170a98d501144f4
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-02 12:56:04 +0100

    remove unused code

--- spec/localhost/emacs_spec.rb
+++ spec/localhost/emacs_spec.rb
@@ -6,22 +6,14 @@ describe 'emacs installation' do
     its(:exit_status) { should eq 0 }
   end
 
-  describe 'read file with emacs' do
+  describe 'add a file and write text into it with emacs' do
     before do
       system('emacs -batch ./spec/files/flower.txt --eval \'(insert "Butterblume")\' -f save-buffer')
     end
-    #describe command('emacs --batch -l butterblume.txt') do
-      #its(:stdout) { should match 'Butterblume' }
-    #end
 
     describe command('cat ./spec/files/flower.txt') do
       its(:stdout) { should match 'Butterblume' }
     end
   end
-
-#https://www.gnu.org/software/emacs/manual/html_node/emacs/Command-Example.html#Command-Example
-#
-#https://www.fnal.gov/docs/products/emacs/emacs/emacs_36.html
-
 
 end
