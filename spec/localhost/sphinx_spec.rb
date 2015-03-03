3bf6b2fc9b11e8f76feb9d7ad47963c67916da25
carlad <carla@travis-ci.org>
2015-03-03 12:37:06 +0100

    update paths

--- spec/localhost/sphinx_spec.rb
+++ spec/localhost/sphinx_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe 'sphinx installation' do
 
-  describe command('cd ../../usr/local/sphinx-22.6/bin; ./indexer') do
+  describe command('/usr/local/sphinx-2.2.6/bin/indexer') do
     its(:stdout) { should match 'Sphinx 2.2.6-id64-release' }
   end
 
-  describe command('cd ../../usr/local/sphinx-2.1.9/bin; ./indexer') do
+  describe command('/usr/local/sphinx-2.1.9/bin/indexer') do
     its(:stdout) { should match 'Sphinx Sphinx 2.1.9-release' }
   end
 
-  describe command('cd ../../usr/local/sphinx-2.0.10/bin; ./indexer') do
+  describe command('/usr/local/sphinx-2.0.10/bin/indexer') do
     its(:stdout) { should match 'Sphinx Sphinx 2.0.10-release' }
   end
 end
