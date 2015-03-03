310bb29f3cd7a4f7e2b28138242bb92c28d6f124
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-03 15:21:26 +0100

    add a comma between the output

--- spec/localhost/mingw32_spec.rb
+++ spec/localhost/mingw32_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'minGW' do
   describe command ('gcc') do
-    its(:stdout) { should include('gcc: no input files') }
+    its(:stdout) { should include('gcc:', 'no input files') }
   end
 end
