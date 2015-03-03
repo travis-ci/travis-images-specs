cf134d373a6e8735ef2e1c6003635687c0e508f5
carlad <carla@travis-ci.org>
2015-03-03 12:43:39 +0100

    fix spelling error

--- spec/localhost/sphinx_spec.rb
+++ spec/localhost/sphinx_spec.rb
@@ -7,10 +7,10 @@ describe 'sphinx installation' do
   end
 
   describe command('/usr/local/sphinx-2.1.9/bin/indexer') do
-    its(:stdout) { should match 'Sphinx Sphinx 2.1.9-release' }
+    its(:stdout) { should match 'Sphinx 2.1.9-release' }
   end
 
   describe command('/usr/local/sphinx-2.0.10/bin/indexer') do
-    its(:stdout) { should match 'Sphinx Sphinx 2.0.10-release' }
+    its(:stdout) { should match 'Sphinx 2.0.10-release' }
   end
 end
