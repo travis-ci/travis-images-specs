67581fbd3b250719f1484b707b8ab220df9cdbb7
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-05 13:43:46 +0100

    add the sudo true and false to the bash script

--- bin/test_languages.sh
+++ bin/test_languages.sh
@@ -5,24 +5,29 @@
 
 # run this script by passing languages as options
 # eg 'test_languages.sh ruby python php'
-# will send 3 build requests, one for each language 
- 
+# will send 3 build requests, one for each language
+
 set -e
- 
+
 langs=$@
 url=https://api.travis-ci.org/requests
- 
-for lang in $langs; do
-  data='
-    {
-      "request": {
-        "config": { "language": "'$lang'" },
-        "message": "Test language '$lang'",
-        "repository": { "owner_name": "travis-ci", "name": "travis-images-specs" }
-      }
-    }
-  '
 
-  echo "Triggering requests for testing the $lang image"
-  echo $(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: token $TRAVIS_ACCESS_TOKEN" -d "$data" $url)
+
+for sudo in "true false"; do
+  for lang in $langs; do
+    data='
+      {
+        "request": {
+          "config": { "language": "'$lang'" },
+          "sudo": "'$sudo'",
+          "message": "Test language '$lang'",
+          "repository": { "owner_name": "travis-ci", "name": "travis-images-specs" }
+        }
+      }
+    '
+
+    echo "Triggering requests for testing the $lang image"
+    echo $(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: token $TRAVIS_ACCESS_TOKEN" -d "$data" $url)
+    echo $sudo
+  done
 done
