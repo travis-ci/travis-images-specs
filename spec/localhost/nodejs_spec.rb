1f2b2759b1d653c05f068dfae24cf1319a84f6e3
carlad <carla@travis-ci.org>
2015-03-06 16:56:42 +0100

    remove blank space

--- spec/localhost/nodejs_spec.rb
+++ spec/localhost/nodejs_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe 'nodejs installation ' do
-
+describe 'nodejs installation' do
   describe command('node -v') do
     its(:stdout) { should match 'v0.10.36'}
   end
