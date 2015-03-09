554b56cc30cc21b302528598d6b9858fd2bbaca7
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-09 15:42:08 +0100

    add node-server again

--- spec/localhost/nodejs_spec.rb
+++ spec/localhost/nodejs_spec.rb
@@ -5,7 +5,7 @@ describe 'nodejs installation' do
     its(:stdout) { should match 'v0.10.36'}
   end
 
-  #describe command('node ./spec/files/node_server.js') do
-    #its(:stdout) { should match 'Server running at http://127.0.0.1:1337/'}
-  #end
+  describe command('node ./spec/files/node_server.js') do
+    its(:stdout) { should match 'Server running at http://127.0.0.1:1337/'}
+  end
 end
