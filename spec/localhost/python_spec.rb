ca8d60e473aa33419b3605c212c0d4fcbbfcef42
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-09 13:58:48 +0100

    back to old version

--- spec/localhost/python_spec.rb
+++ spec/localhost/python_spec.rb
@@ -6,13 +6,13 @@ describe 'python and pip installation ' do
     its(:exit_status) { should eq 0 }
   end
 
-  describe command('pip --version'), sudo: true do
+  describe command('pip --version') do
     its(:stdout) { should match 'pip 6.0.7'}
   end
 
-  describe command('pip --version'), sudo: false do
-    its(:stdout) { should match 'pip 6.0.8'}
-  end
+  #describe command('pip --version'), sudo: false do
+    #its(:stdout) { should match 'pip 6.0.8'}
+  #end
 
 end
 
