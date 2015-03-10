6d57887592f0a59b60be789ddfc34a79410c7fcf
carlad <carla@travis-ci.org>
2015-03-10 12:08:54 +0100

    update sysctl spec for docker

--- spec/localhost/sysctl_spec.rb
+++ spec/localhost/sysctl_spec.rb
@@ -8,6 +8,6 @@ end
 
 describe 'sysctl commands are executed' do
   describe command('sysctl -a; sleep 5') do
-    its(:stdout) { should include('kernel.sched_child_runs_first', 'kernel.sched_min_granularity_ns', 'net.core.warnings', 'cpt.suspend_timeout_sec' ) }
+    its(:stdout) { should include('kernel.sched_child_runs_first', 'kernel.sched_min_granularity_ns') }
   end
 end
