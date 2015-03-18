7df3cedde727db1ce618942b846f19e2d75575df
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-18 15:13:32 +0100

    add python version output

--- spec/localhost/python_spec.rb
+++ spec/localhost/python_spec.rb
@@ -2,20 +2,32 @@ require 'spec_helper'
 
 describe 'python and pip installation ' do
 
-  describe command('python --version') do
-    its(:exit_status) { should eq 0 }
+  describe 'python version' do
+    before :all do
+      system('python --version')
+    end
+
+    describe command('python --version') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 
-  describe command('pip --version') do
-    its(:exit_status) { should eq 0 }
+  describe 'pip version' do
+    before :all do
+      system('pip --version')
+    end
+
+    describe command('pip --version') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 
-end
-
-describe 'Python command is executed' do
-  describe command('python -c "print 123 + 123"') do
-    its(:stdout) { should match '246' }
+  describe 'python command is executed' do
+    describe command('python -c "print 123 + 123"') do
+      its(:stdout) { should match '246' }
+    end
   end
+
 end
 
 
