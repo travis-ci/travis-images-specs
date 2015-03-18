a21378c5e4590019a19a47e3418dcb1cbd97bc54
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-18 12:59:32 +0100

    add ruby version output

--- spec/localhost/ruby_spec.rb
+++ spec/localhost/ruby_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
-describe 'Ruby installation' do
-  describe command('rvm list default') do
-    its(:stdout) { should match '1.9.3' }
+describe 'ruby installation' do
+
+  describe 'ruby version' do
+    before :all do
+      system('ruby -v')
+    end
+
+    describe command('ruby -v') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 
-  describe command('rvm list known') do
-    its(:stdout) { should match '1.9.3' }
-  end
-
-  describe 'Ruby command is executed' do
+  describe 'ruby command is executed' do
     describe command('ruby -e \'puts \"Konstanin broke all the things!\"\'') do
       its(:stdout) { should match 'Konstanin broke all the things' }
     end
   end
+
 end
