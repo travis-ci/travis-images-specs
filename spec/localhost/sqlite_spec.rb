d1457a2fb594dba6577897085cc79fae9d5f2eda
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-18 11:28:52 +0100

    add version output to sqlite3 tests

--- spec/localhost/sqlite_spec.rb
+++ spec/localhost/sqlite_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe 'sqlite installation' do
-  describe command('sqlite3 -version') do
-    its(:stdout) { should match '3.7.9' }
+
+  describe 'sqlite version' do
+    before :all do
+      system('sqlite3 -version')
+    end
+
+    describe command('sqlite3 -version') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 
   describe 'sqlite commands are executed' do
-    describe command('sqlite3 test.db "CREATE TABLE Cars(Id INTEGER PRIMARY KEY, Name TEXT, Price INTEGER); 
-                      INSERT INTO Cars VALUES(1,\'Audi\',52642); 
+    describe command('sqlite3 test.db "CREATE TABLE Cars(Id INTEGER PRIMARY KEY, Name TEXT, Price INTEGER);
+                      INSERT INTO Cars VALUES(1,\'Audi\',52642);
                       SELECT * FROM Cars;"') do
       its(:stdout) { should match '1|Audi|52642' }
     end
