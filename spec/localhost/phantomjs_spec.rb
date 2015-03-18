5beeda261d725c67734e6c2b6746fc5ff90fb6bf
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-18 15:26:13 +0100

    add phantonjs version output

--- spec/localhost/phantomjs_spec.rb
+++ spec/localhost/phantomjs_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe 'phantomjs installation' do
 
-  describe command('phantomjs -v') do
-    its(:exit_status) { should eq 0 }
+  describe 'phantomjs version' do
+    before :all do
+      system('phantomjs -v')
+    end
+
+    describe command('phantomjs -v') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 
-  describe command ('phantomjs ./spec/files/phantomjs_test_google.js') do
-    its(:stdout) { should match 'Status: success' }
+  describe 'phantomjs commands' do
+    describe command ('phantomjs ./spec/files/phantomjs_test_google.js') do
+      its(:stdout) { should match 'Status: success' }
+    end
   end
 
 end
