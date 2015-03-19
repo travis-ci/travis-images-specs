ad4b8541bd4ef7d9f21969260515548d8b356d2b
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-19 15:48:31 +0100

    add emacs version output

--- spec/localhost/emacs_spec.rb
+++ spec/localhost/emacs_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 describe 'emacs installation' do
 
-  describe command('emacs --version') do
-    its(:exit_status) { should eq 0 }
+  describe 'emacs version' do
+    before :all do
+      system('emacs --version | head -1')
+    end
+
+    describe command('emacs --version') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 
   describe 'add a file and write text into it with emacs' do
