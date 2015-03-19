6c39c2d62261f4d265204a016e86e09cb7357bdb
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-19 14:25:11 +0100

    add gcc version output

--- spec/localhost/mingw32_spec.rb
+++ spec/localhost/mingw32_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe 'minGW' do
+
+  describe 'gcc version' do
+    before :all do
+      system('gcc -v 2>&1 | tail -1')
+    end
+  end
+
   describe command ('gcc') do
     its(:stdout) { should include('gcc:', 'no input files') }
   end
