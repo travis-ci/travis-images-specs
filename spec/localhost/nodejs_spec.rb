7d2ff288ab270cc04bdb2b258f8038189e5c9af7
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-20 15:17:14 +0100

    Update nodejs_spec.rb

--- spec/localhost/nodejs_spec.rb
+++ spec/localhost/nodejs_spec.rb
@@ -13,10 +13,6 @@ describe 'nodejs installation' do
   end
 
   describe 'node commands' do
-    describe command('node ./spec/files/node_server.js &') do
-      its(:stdout) { should match 'Server running at http://127.0.0.1:1337/'}
-    end
-
     describe command('node -e "console.log(\'Konstantin broke all the thingz\')"') do
       its(:stdout) { should match 'Konstantin broke all the thingz'}
     end
