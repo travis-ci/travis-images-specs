1d53c8691d16df1823bc070a0d7f44c006536b31
Dan Buch <daniel.buch@gmail.com>
2015-09-30 14:41:08 -0600

    Install a smaller package for apt testing

--- spec/localhost/apt_spec.rb
+++ spec/localhost/apt_spec.rb
@@ -4,12 +4,12 @@ describe 'apt installation', mega: true, standard: true, minimal: true do
   end
 
   describe 'apt commands', sudo: true do
-    describe command('sudo apt-get update') do
-      its(:stdout) { should match 'Reading package lists...' }
+    describe command('sudo apt-get -y update') do
+      its(:stdout) { should match(/Reading package lists/) }
     end
 
-    describe command('sudo apt-get install default-jdk') do
-      its(:stdout) { should match 'Setting up default-jdk' }
+    describe command('sudo apt-get -y install language-pack-ar-base') do
+      its(:stdout) { should match(/Setting up /) }
     end
   end
 end
