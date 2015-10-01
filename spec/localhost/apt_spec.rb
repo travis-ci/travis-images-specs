d27c5bbb2d1bed109256476cb34266315a888477
Dan Buch <daniel.buch@gmail.com>
2015-10-01 11:50:44 -0400

    Attempt to fix apt spec ?

--- spec/localhost/apt_spec.rb
+++ spec/localhost/apt_spec.rb
@@ -1,10 +1,14 @@
 describe 'apt installation', mega: true, standard: true, minimal: true do
+  before :all do
+    system('sudo apt-get update -yqq', [:out, :err] => '/dev/null')
+  end
+
   describe command('apt-get -v') do
     its(:exit_status) { should eq 0 }
   end
 
   describe 'apt commands', sudo: true do
-    describe command('sudo apt-get -y update') do
+    describe command('sudo apt-get update -y') do
       its(:stdout) { should match(/Reading package lists/) }
     end
 
