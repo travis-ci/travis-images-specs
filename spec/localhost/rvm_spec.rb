c80527e5d068c464eed426316d283d66ef178f93
Dan Buch <daniel.buch@gmail.com>
2015-10-01 21:58:50 -0400

    Make more assertions about RVM installation

--- spec/localhost/rvm_spec.rb
+++ spec/localhost/rvm_spec.rb
@@ -4,6 +4,12 @@ describe 'rvm installation', mega: true, standard: true, minimal: true do
     its(:exit_status) { should eq 0 }
   end
 
+  describe file('/usr/local/rvm/user/db'), standard: :todo do
+    it { should exist }
+    it { should be_writable.by_user('travis') }
+    it { should be_readable.by_user('travis') }
+  end
+
   describe 'rvm commands' do
     describe command('rvm list') do
       its(:stdout) { should include('rvm rubies', 'current') }
