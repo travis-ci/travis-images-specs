74788e1b19b9b59fd310706cc1aa82f9aeeb1710
Dan Buch <daniel.buch@gmail.com>
2015-10-01 22:27:08 -0400

    Assert different paths on legacy/current
    given different cookbooks in use

--- spec/localhost/rvm_spec.rb
+++ spec/localhost/rvm_spec.rb
@@ -4,15 +4,21 @@ describe 'rvm installation', mega: true, standard: true, minimal: true do
     its(:exit_status) { should eq 0 }
   end
 
-  describe file('/usr/local/rvm/user/db'), standard: :todo do
-    it { should exist }
-    it { should be_writable }
-    it { should be_readable }
-  end
-
   describe 'rvm commands' do
     describe command('rvm list') do
       its(:stdout) { should include('rvm rubies', 'current') }
     end
   end
+end
+
+describe file('/usr/local/rvm/user/db'), mega: true, minimal: true do
+  it { should exist }
+  it { should be_writable }
+  it { should be_readable }
+end
+
+describe file(::File.expand_path('~/.rvm/user/db')), standard: true do
+  it { should exist }
+  it { should be_writable }
+  it { should be_readable }
 end
