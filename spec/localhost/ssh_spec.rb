4a41fa249062f760bb774bc40591d72910b686ab
Dan Buch <daniel.buch@gmail.com>
2015-10-01 23:38:31 -0400

    Use `:>` instead of nonexistent `:gt` method
    ___RUN_THE_SUITE___

--- spec/localhost/ssh_spec.rb
+++ spec/localhost/ssh_spec.rb
@@ -2,7 +2,7 @@ describe 'ssh access', mega: true, standard: true, minimal: true do
   %w(known_hosts authorized_keys).each do |basename|
     describe file(::File.expand_path("~/.ssh/#{basename}")) do
       it { should exist }
-      its(:size) { should be gt 0 }
+      its(:size) { should be > 0 }
       it { should be_readable }
       it { should be_writable }
     end
